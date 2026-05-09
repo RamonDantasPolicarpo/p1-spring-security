e4e1e27e067225989384acca496dc7f6eebc645b
Ramon Dantas Policarpo <ramonpolicarpo36@gmail.com>
2026-05-08 22:55:17 -0300

    Init commit

---
+++ src/main/java/dev/ramondp/p1_spring_security/P1SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package dev.ramondp.p1_spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P1SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P1SpringSecurityApplication.class, args);
+	}
+
+}
